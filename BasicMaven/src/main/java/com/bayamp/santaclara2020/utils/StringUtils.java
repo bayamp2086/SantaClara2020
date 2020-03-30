3a806f4c09d27f9e97b7d86df27a600577bff3af
Ranjini <ranjinirajaram2020@gmail.com>
2020-03-29 19:46:16 -0700

    1.Added reverseString() 2.Refactored the code based on code review comments.

--- BasicMaven/src/main/java/com/bayamp/santaclara2020/utils/StringUtils.java
+++ BasicMaven/src/main/java/com/bayamp/santaclara2020/utils/StringUtils.java
@@ -3,7 +3,18 @@ package com.bayamp.santaclara2020.utils;
 public class StringUtils {
 
     public StringUtils() throws Exception{
-        throw new Exception("I dont like you at all");
+
+        throw new Exception("I don't like you at all");
+
+    }
+    public static String reverseString(String str) {
+
+        if(str==null) {
+            return null;
+        }
+        StringBuffer buff = new StringBuffer(str);
+        return buff.reverse().toString();
+
     }
 
     public static boolean isPalindrome(String input) throws Exception {
