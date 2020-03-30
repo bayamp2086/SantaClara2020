bb16a9a3e7e8256bf0ad96570f848143a9c538d3
Ranjini <ranjinirajaram2020@gmail.com>
2020-03-29 17:25:06 -0700

    Generalized getRandomStringOfLen() method to take numberOfDigits as input and generate a random number according to length requested.

--- BasicMaven/src/main/java/com/bayamp/santaclara2020/utils/RandomUtil.java
+++ BasicMaven/src/main/java/com/bayamp/santaclara2020/utils/RandomUtil.java
@@ -8,73 +8,69 @@ import java.util.Random;
 
 public class RandomUtil {
 
-    //private static final String DATE_FORMAT = "MM/dd/YYYY HH:mm";
+	// private static final String DATE_FORMAT = "MM/dd/YYYY HH:mm";
 
-    public static long getRandomLongNumber( ) {
-        Random randomObject = new Random();
-        long randomNumber = randomObject.nextLong() ;
+	public static long getRandomLongNumber() {
+		Random randomObject = new Random();
+		long randomNumber = randomObject.nextLong();
 
-        return randomNumber;
+		return randomNumber;
 
-    }
+	}
 
-    /**
-     * if parameter value is 1 (return any random number from 0-9)
-     * if parameter value is 2 (return any random number from 10-99)
-     * if parameter value is 3 (return any random number from 100-999)
-     * @param numberOfDigits
-     * @return
-     */
-    public static String getRandomStringOfLen(int numberOfDigits ) {
-        Random randomObject = new Random();
+	/**
+	 * if parameter value is 1 (return any random number from 0-9)
+	 * if parameter value is 2 (return any random number from 10-99)
+	 * if parameter value is 3 (return any random number from 100-999)
+	 * @param numberOfDigits
+	 * @return
+	 */
+	public static int getRandomStringOfLen(int numberOfDigits) {
 
-        int nextRandom = randomObject.nextInt(1000); //0-999
+		int m = (int) Math.pow(10, numberOfDigits - 1); // 0-999
 
-        String str = String.format("%03d",nextRandom);
+		return m + new Random().nextInt(9 * m);
 
-        return str;
+	}
 
-    }
+	public static long getRandomNumberAsMilliSeconds() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return System.currentTimeMillis();
+	}
 
-    public static long getRandomNumberAsMilliSeconds( ) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        return System.currentTimeMillis();
-    }
+	public static String getRandomSringAsMilliSeconds() {
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		long random = System.currentTimeMillis();
 
+		return Long.toString(random);
 
-    public static String getRandomSringAsMilliSeconds( ) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        long random = System.currentTimeMillis();
+	}
 
-        return Long.toString(random);
+	public static String getRandomFirstName() {
 
-    }
+		return "TestUserF" + getRandomNumberAsMilliSeconds();
+	}
 
-    public static String getRandomFirstName() {
+	public static String getDate() {
 
-        return "TestUserF"+getRandomNumberAsMilliSeconds();
-    }
+		Date date = new Date();
+		SimpleDateFormat format = new SimpleDateFormat(Constants.DATE_FORMAT);
+		return format.format(date);
+	}
 
-    public static String getDate() {
-
-        Date date = new Date();
-        SimpleDateFormat format = new SimpleDateFormat(Constants.DATE_FORMAT);
-        return format.format(date);
-    }
-
-    public static String getDate(String strFormat) {
-        Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat(strFormat);
-        return dateFormat.format(date);
-    }
+	public static String getDate(String strFormat) {
+		Date date = new Date();
+		SimpleDateFormat dateFormat = new SimpleDateFormat(strFormat);
+		return dateFormat.format(date);
+	}
 
 	public static String generateRandomPhoneNumber() {
 		return null;
