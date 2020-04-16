5ff244690d60bcb827bbc0bb0b55f78551a8f31c
Ranjini <ranjinirajaram2020@gmail.com>
2020-04-16 16:03:56 -0700

    1.Created BayampMailBoxFunctionalityTests class 2.Added composeMailTest and inboxMailTest test case methods

--- BasicMaven/src/test/java/com/bayamp/santaclara2020/apitests/BayampLoginTests.java
+++ BasicMaven/src/test/java/com/bayamp/santaclara2020/apitests/BayampLoginTests.java
@@ -1,6 +1,7 @@
-package com.bayamp.santaclara2020.dummy.selenium.tests;
+package com.bayamp.santaclara2020.apitests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -41,7 +42,7 @@ public class BayampLoginTests {
         driver.get(APP_URL);
         userField = driver.findElement(By.id("user"));
         passwordField = driver.findElement(By.id("pass"));
-        submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
 
         // driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
 
@@ -66,13 +67,57 @@ public class BayampLoginTests {
     }
 
     @Test
-    public void negativeLoginTest() {
+    public void negativeLoginTest() throws InterruptedException {
         driver.get(APP_URL);
         userField = driver.findElement(By.id("user"));
         passwordField = driver.findElement(By.id("pass"));
-        submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
-        userField.sendKeys("user12@bayamp.com");
-        //driver.close();
+        submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+
+        String expectedValidation = "You must specify a username to log in.";
+        Thread.sleep(1000);
+        By divElement = By.id("login-status-message");
+        WebElement errorMessageWebElement = driver.findElement(divElement);
+        String valueOfTheMessage = errorMessageWebElement.getText();
+
+        Assert.assertEquals(valueOfTheMessage, expectedValidation);
+    }
+
+   @Test
+    public void maskedPasswordTest() {
+        driver.get(APP_URL);
+        passwordField = driver.findElement(By.id("pass"));
+
+        boolean isMasked = passwordField.getAttribute("type").equals("password");
+
+        Assert.assertTrue(isMasked, "The password field is not masked");
+    }
+
+    @Test
+    public void upperCaseLoginTest() throws InterruptedException {
+
+        String expectedUserEmail = "user1@bayamp.com";
+        String expectedLogOutText = "Logout";
+
+        driver.get(APP_URL);
+        userField = driver.findElement(By.id("user"));
+        passwordField = driver.findElement(By.id("pass"));
+        submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+
+        userField.clear();
+        userField.sendKeys("USER1@BAYAMP.COM");
+
+        //userField.sendKeys(Keys.HOME, Keys.chord(Keys.SHIFT, Keys.END), "USER1@BAYAMP.COM");
+
+        passwordField.sendKeys("user1");
+
+        submitButton.click();
+
+        String actualUserEmail = driver.findElement(By.id("lblUserNameTxt")).getText();
+        String actualLogOutText = driver.findElement(By.id("lblLogout")).getText();
+
+        Assert.assertEquals(actualUserEmail, expectedUserEmail);
+        Assert.assertEquals(actualLogOutText, expectedLogOutText);
     }
 }
 
