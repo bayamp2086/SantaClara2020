eed0f923f92c452206b7634ebaf3fba0827218a9
Ranjini <ranjinirajaram2020@gmail.com>
2020-04-16 23:44:21 -0700

    Combined constants used in registration program and DummyBase class program in  Constants.java

--- BasicMaven/src/main/java/com/bayamp/santaclara2020/generic/Constants.java
+++ BasicMaven/src/main/java/com/bayamp/santaclara2020/generic/Constants.java
@@ -1,4 +1,15 @@
 package com.bayamp.santaclara2020.generic;
 
 public class Constants {
+    public static final String CONFIG_FILE_PATH = "src/test/resources/config.properties";
+    public static final String UI_CONFIG_FILE_PATH = "src/test/resources/uiconfig.properties";
+    public static final String API_CONFIG_FILE_PATH = "src/test/resources/apiconfig.properties";
+
+    public static final String TEST_DATA_FOLDER_LOCATION = "src/main/resources/data/address.csv";
+    public static final String RELATIVE_FILE_PATH = "src/main/resources/data/myfile.txt";
+
+    public static final String DATE_FORMAT = "MM/dd/YYYY HH:mm";
+    public static String AUTOUSER_FIRSTNAME = "FAutomationUser";
+    public static String AUTOUSER_LASTNAME = "FAutomationUser";
+    public static String AUTOUSERID = "FAutomationUser";
 }
