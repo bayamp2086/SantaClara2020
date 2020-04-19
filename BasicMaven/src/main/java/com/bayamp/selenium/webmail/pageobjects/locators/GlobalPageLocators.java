a7840b214a9e81309324faaf726fb8673618964d
Ranjini <ranjinirajaram2020@gmail.com>
2020-04-19 12:51:00 -0700

    Created GlobalPageLocators.java with sample locators

---
+++ BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/locators/GlobalPageLocators.java
@@ -0,0 +1,9 @@
+package com.bayamp.selenium.webmail.pageobjects.locators;
+
+import org.openqa.selenium.By;
+
+public class GlobalPageLocators {
+    private static final By SUBMIT_BUTTON_CSS_LOCATOR = By.cssSelector("button[type='submit']");
+    private static final By CANCEL_BUTTON_CSS_LOCATOR = By.cssSelector("button[type='submit']");
+    private static final By RESET_BUTTON_CSS_LOCATOR = By.cssSelector("button[type='submit']");
+}
