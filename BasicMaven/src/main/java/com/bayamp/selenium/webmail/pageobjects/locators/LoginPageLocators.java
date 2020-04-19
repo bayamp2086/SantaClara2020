04413a9b65d76743b5222cf462a00984ad736faf
Ranjini <ranjinirajaram2020@gmail.com>
2020-04-19 12:51:53 -0700

    Created LoginPageLocators.java with all field locators

---
+++ BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/locators/LoginPageLocators.java
@@ -0,0 +1,14 @@
+package com.bayamp.selenium.webmail.pageobjects.locators;
+
+import org.openqa.selenium.By;
+
+public class LoginPageLocators {
+
+    public static final By USER_FIELD_ID_LOCATOR = By.id("user");
+    public static final By PASSWORD_FIELD_ID_LOCATOR = By.id("pass");
+    public static final By LOGIN_BUTTON_CSS_LOCATOR = By.cssSelector("button[type='submit']");
+
+    public static final By LOGOUT_FIELD_ID_LOCATOR = By.xpath("//*[text()='Logout']");
+    public static final By ERROR_MESSAGE_ID_LOCATOR= By.xpath("//*[text()='You must specify a username to log in.']");
+
+}
