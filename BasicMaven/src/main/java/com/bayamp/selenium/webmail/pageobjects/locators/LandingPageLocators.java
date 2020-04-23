7a7557c10835d574e2ac9a5e5d2e16e90c3ad1e0
Ranjini <ranjinirajaram2020@gmail.com>
2020-04-22 18:42:34 -0700

    Created LandingPageLocators.java and included logout field and user email field in the same.

---
+++ BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/locators/LandingPageLocators.java
@@ -0,0 +1,9 @@
+package com.bayamp.selenium.webmail.pageobjects.locators;
+
+import org.openqa.selenium.By;
+
+public class LandingPageLocators {
+
+    public static final By LOGOUT_FIELD_ID_LOCATOR = By.xpath("//*[text()='Logout']");
+    public static final By USER_EMAIL_FIELD_ID_LOCATOR = By.id("lblUserNameTxt");
+}
