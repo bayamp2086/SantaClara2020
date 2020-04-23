7798a4ff4d1cd2184ed14e3bbad9cc2938d482e6
Ranjini <ranjinirajaram2020@gmail.com>
2020-04-22 18:40:49 -0700

    removed logout field in LoginpageLocators

--- BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/locators/LoginPageLocators.java
+++ BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/locators/LoginPageLocators.java
@@ -7,8 +7,6 @@ public class LoginPageLocators {
     public static final By USER_FIELD_ID_LOCATOR = By.id("user");
     public static final By PASSWORD_FIELD_ID_LOCATOR = By.id("pass");
     public static final By LOGIN_BUTTON_CSS_LOCATOR = By.cssSelector("button[type='submit']");
-
-    public static final By LOGOUT_FIELD_ID_LOCATOR = By.xpath("//*[text()='Logout']");
     public static final By ERROR_MESSAGE_ID_LOCATOR= By.xpath("//*[text()='You must specify a username to log in.']");
 
 }
