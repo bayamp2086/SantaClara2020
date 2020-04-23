d36b8fe748a61a71eeae611075d9bbd4d37e5d93
Ranjini <ranjinirajaram2020@gmail.com>
2020-04-22 18:39:45 -0700

    Modified login positive test and negative test according Page Object Model

--- BasicMaven/src/main/java/com/bayamp/selenium/webmail/tests/LoginTests.java
+++ BasicMaven/src/main/java/com/bayamp/selenium/webmail/tests/LoginTests.java
@@ -1,33 +1,41 @@
 package com.bayamp.selenium.webmail.tests;
 
 import com.bayamp.selenium.webmail.base.BaseWebUI;
+import com.bayamp.selenium.webmail.pageobjects.LandingPage;
 import com.bayamp.selenium.webmail.pageobjects.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends BaseWebUI {
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private String userName,password;
 
-    @Test
-    public void loginPositiveTest() {
+    @BeforeMethod
+    public void setUp() throws Exception {
 
-        WebDriver driver = getDriver();//this driver object is v.important as it holds the state of entire browser session.
-
-        LoginPage loginPage = new LoginPage(driver);
-        boolean check = loginPage.login("user1@bayamp.com", "user1");
-
-        Assert.assertTrue(check,"Login Failed");
+        driver = getDriver();
+        loginPage =  new LoginPage(driver);
 
     }
+
     @Test
-    public void loginNegativeTest() {
+    public void loginPositiveTest() throws Exception {
 
-        WebDriver driver = getDriver();//this driver object is v.important as it holds the state of entire browser session.
+       userName = getUserName();
+       password = getPassword();
+        LandingPage landingPage = loginPage.login(userName,password);
+        Assert.assertTrue(landingPage.isLogOutElementPresent());
 
-        LoginPage loginPage = new LoginPage(driver);
-        boolean check = loginPage.login();
+    }
 
-        Assert.assertTrue(check,"Login Failed");
+    @Test
+    public void loginNegativeTest() throws Exception {
+
+        LandingPage landingPage = loginPage.login("dfff","");
+        Assert.assertFalse(landingPage.isLogOutElementPresent());
 
     }
 }
