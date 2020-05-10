ef6911d94a4a5abeccd49f496f5f51e77cd1197b
Ranjini <ranjinirajaram2020@gmail.com>
2020-05-10 14:29:32 -0700

    Created RestApiLocators class to save all endpoints in a variable.

---
+++ BasicMaven/src/main/java/com/bayamp/webservices/restapi/locators/RestApiLocators.java
@@ -0,0 +1,11 @@
+package com.bayamp.webservices.restapi.locators;
+
+public class RestApiLocators {
+
+    public static final String BASE_URI = "http://bayamprestapp.herokuapp.com";
+    public static final String POST_ENDPOINT = "https://bayamprestapp.herokuapp.com/bayamp/create";
+    public static final String PUT_ENDPOINT = "https://bayamprestapp.herokuapp.com/bayamp/update/";
+    public static final String GET_ENDPOINT = "https://bayamprestapp.herokuapp.com/bayamp/engineers";
+    public static final String DELETE_ENDPOINT = "https://bayamprestapp.herokuapp.com/bayamp/delete/";
+
+}
