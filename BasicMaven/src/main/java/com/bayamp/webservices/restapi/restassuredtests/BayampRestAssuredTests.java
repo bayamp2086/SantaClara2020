ae28080fd4061e281fce5f5ca7819ac7919162c6
Ranjini <ranjinirajaram2020@gmail.com>
2020-05-10 14:26:35 -0700

    Created BayampRestAssuredTests using RestAssured and RestAPI endpoints.

---
+++ BasicMaven/src/main/java/com/bayamp/webservices/restapi/restassuredtests/BayampRestAssuredTests.java
@@ -0,0 +1,93 @@
+package com.bayamp.webservices.restapi.restassuredtests;
+
+import com.bayamp.webservices.restapi.locators.RestApiLocators;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class BayampRestAssuredTests {
+
+    @Test
+    public static Response createEngineerResource() {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("name", "RestAssuredPost");
+
+        return
+                given()
+                        .contentType("application/json")
+                        .body(jsonObj.toString())
+                        .when()
+                        .request("POST", RestApiLocators.POST_ENDPOINT)
+                        .then()
+                        .statusCode(201)
+                        .statusLine(containsString("Created"))
+                        .extract().response();
+        //System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public static Response updateEngineerResource(String engineerId) {
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("name", "RestAssuredPut");
+
+        return given()
+                .contentType("application/json")
+                .body(jsonObj.toString())
+                .when()
+                .request("PUT", RestApiLocators.PUT_ENDPOINT + engineerId)
+                .then()
+                .statusCode(200)
+                .statusLine(containsString("OK"))
+                .extract().response();
+        //System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public static Response deleteEngineerResource(String engineerId) {
+
+        return given()
+                .when()
+                .request("DELETE", RestApiLocators.DELETE_ENDPOINT + engineerId)
+                .then()
+                .statusCode(200)
+                .statusLine(containsString("OK"))
+                .extract().response().andReturn();
+        //System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public static Response getEngineerWithIdResource(String engineerId) {
+
+        return
+                when()
+                        .get(RestApiLocators.GET_ENDPOINT + "/" + engineerId)
+                        .then()
+                        .assertThat()
+                        .statusCode(200)
+                        .statusLine(containsString("OK"))
+                        .and()
+                        .body("name", equalTo("TrialPost"))
+                        .extract().response();
+        //System.out.println(response.getBody().asString());
+    }
+
+    @Test
+    public static Response getAllEngineers() {
+
+        return given()
+                .contentType("application/json")
+                .get(RestApiLocators.GET_ENDPOINT)
+                .then()
+                .assertThat()
+                .statusCode(200).and()
+                .statusLine(containsString("OK"))
+                .extract().response();
+        //System.out.println(response.getBody().asString());
+    }
+}
