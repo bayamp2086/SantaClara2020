7122a110a21e077b65a39b835dd67324f2e9f6d2
Ranjini <ranjinirajaram2020@gmail.com>
2020-05-16 01:12:55 -0700

    created login() and isErrorMessagePresent() in LoginPage.java

--- BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/LoginPage.java
+++ BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/LoginPage.java
@@ -1,9 +1,12 @@
 package com.bayamp.selenium.webmail.pageobjects;
 
 import com.bayamp.selenium.webmail.pageobjects.locators.LoginPageLocators;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 public class LoginPage {
     private WebDriver driver;
@@ -16,7 +19,7 @@ public class LoginPage {
 
     }
 
-    public LandingPage login(String user,String password) {
+    public LandingPage loginSuccessful(String user,String password) {
 
         driver.findElement(LoginPageLocators.USER_FIELD_ID_LOCATOR).sendKeys(user);
         driver.findElement(LoginPageLocators.PASSWORD_FIELD_ID_LOCATOR).sendKeys(password);
@@ -25,6 +28,24 @@ public class LoginPage {
         LandingPage landingPage = new LandingPage(driver);
         return landingPage;
 
+    }
+    public void login(String user,String password) {
+
+        driver.findElement(LoginPageLocators.USER_FIELD_ID_LOCATOR).sendKeys(user);
+        driver.findElement(LoginPageLocators.PASSWORD_FIELD_ID_LOCATOR).sendKeys(password);
+        driver.findElement(LoginPageLocators.LOGIN_BUTTON_CSS_LOCATOR).click();
+    }
+
+    public void isErrorMessagePresent() throws NoSuchElementException, InterruptedException {
+        String expectedErrorMessage = "You must specify a username to log in.";
+
+        Thread.sleep(3000);
+        WebElement errorMessageWebElement = driver.findElement(LoginPageLocators.ERROR_MESSAGE_ID_LOCATOR);
+        String actualErrorMessage = errorMessageWebElement.getText();
+
+        Reporter.log("Perform Validations");
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+        Reporter.log("Validations Complete");
     }
 
     public String getURL() {
