9745cb98e4c3eb86d4ff75bce7f73388fb0961f5
Ranjini <ranjinirajaram2020@gmail.com>
2020-05-16 01:11:36 -0700

    changed the locator to id instead of xpath for error message id locator

--- BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/locators/LoginPageLocators.java
+++ BasicMaven/src/main/java/com/bayamp/selenium/webmail/pageobjects/locators/LoginPageLocators.java
@@ -7,6 +7,6 @@ public class LoginPageLocators {
     public static final By USER_FIELD_ID_LOCATOR = By.id("user");
     public static final By PASSWORD_FIELD_ID_LOCATOR = By.id("pass");
     public static final By LOGIN_BUTTON_CSS_LOCATOR = By.cssSelector("button[type='submit']");
-    public static final By ERROR_MESSAGE_ID_LOCATOR= By.xpath("//*[text()='You must specify a username to log in.']");
-
+    public static final By ERROR_MESSAGE_ID_LOCATOR=  By.id("login-status-message");
+    //By.xpath("//*[text()='You must specify a username to log in.']")
 }
